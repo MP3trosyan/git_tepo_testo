7fca9e887ea498aa5c4b8def7aa469c26e635e0a
Michael Petrosyan <minagutoh@gmail.com>
2024-04-27 12:13:14 +0400

    my first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import Hello from './Hello/Hello'
+
+const jsxElement = (
+  <div>
+    <Hello />
+  </div>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(jsxElement)
